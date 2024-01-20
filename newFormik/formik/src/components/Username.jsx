bbf3029a75b9d597a8a6255e3008a286452e65f1
Ali Tamer Kara <76872565+alitamerkara@users.noreply.github.com>
2024-01-20 17:59:15 +0300

    Update Username.jsx

--- newFormik/formik/src/components/Username.jsx
+++ newFormik/formik/src/components/Username.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 
 const Username = () => {
-  return (
-    <>
-      <div className="inputs">
-        <label>Please enter your username</label>
-        <input name="username" />
-      </div>
-    </>
-  );
+  return <></>;
 };
 
 export default Username;
